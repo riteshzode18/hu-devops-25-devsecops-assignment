12fab4961eae79b4c57a270c59a54b404f9c9727
riteshzode18 <rkishorzode@deloitte.com>
2025-05-27 20:21:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,42 +14,4 @@ provider "aws" {
 
 resource "aws_s3_bucket" "secure_bucket" {
   bucket = "secure-s3-bucket-feature2"
-
-  # Enable server-side encryption
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-
-  # Enable versioning
-  versioning {
-    enabled = true
-  }
-
-  # Add lifecycle configuration
-  lifecycle_rule {
-    id      = "lifecycle"
-    enabled = true
-
-    expiration {
-      days = 30
-    }
-
-    noncurrent_version_expiration {
-      days = 15
-    }
-  }
-
 }
-
-resource "aws_s3_bucket_public_access_block" "secure_bucket_block" {
-  bucket                  = aws_s3_bucket.secure_bucket.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
